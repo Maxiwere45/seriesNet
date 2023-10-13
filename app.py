5404df4d1c34e9547c78059a5551c518c725d15f
Maxiwere 45 <amdjad9857@gmail.com>
2023-10-13 16:46:11 +0200

    TEST

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
-from libs import tfidf
+from flask import Flask, render_template, jsonify
+from libs.tfidf import tfIDF
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
 
 app = Flask(__name__)
-proc = tfidf.tfIDF('./data/data.json', './data/series_names.json', 'json')
 
+procVF = tfIDF('./data/data_VF.json', './data/series_names_VF.json', 'VF')
+procVO = tfIDF('./data/data_VO.json', './data/series_names_VO.json', 'VO')
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/search/VF/<string:id>', methods=['GET'])
+def search(id):
+    return jsonify(procVF.get_series_similaires(str(id)))
 
 
 if __name__ == '__main__':
--- libs/tfidf.py
+++ libs/tfidf.py
@@ -9,12 +9,12 @@ import numpy as np
 
 
 class tfIDF:
-    VECTORIZER = TfidfVectorizer()
-    TF_IDF_MATRIX = None
-    SERIES_NAMES = None
-
-    def __init__(self, data, series_names, type):
+    """
+    Classe permettant de calculer la similarité cosinus entre une requête et les sous-titres
+    """
+    def __init__(self, data, series_names, type_series):
         st = time.time()
+        self.VECTORIZER = TfidfVectorizer()
         with open(series_names, 'r') as f:
             self.SERIES_NAMES = list(json.load(f))
             f.close()
@@ -26,7 +26,7 @@ class tfIDF:
             except Exception as e:
                 f.close()
                 print(e)
-        self.type = type
+        self.type = type_series
         end = time.time()
         print('Temps de chargement des données: ', end - st)
 
@@ -48,7 +48,6 @@ class tfIDF:
         for nom_fichier in os.listdir(folder_path):
             if nom_fichier.endswith('.txt'):
                 chemin_fichier = os.path.join(folder_path, nom_fichier)
-
                 with open(chemin_fichier, 'r', encoding=self.detect_encoding(chemin_fichier)) as fichier:
                     sous_titres_serie = fichier.read()
                     DATA.append(sous_titres_serie)
@@ -58,22 +57,19 @@ class tfIDF:
         self.SERIES_NAMES = SERIES_NAMES
 
         if save:
-            self.save_data('data.json')
+            self.save_data('data_VF.json')
             self.save_data('series_names.json')
 
-    @staticmethod
-    def save_data(file_name):
+    def save_data(self, file_name):
         """
         Sauvegarde les données
-        :param data: données à sauvegarder
-        :param series_names: nom des séries
-        :param type: type de données
+        :param file_name: nom du fichier
         """
         with open(file_name, 'w', encoding='utf-8') as f:
-            json.dump(tfIDF.TF_IDF_MATRIX, f)
+            json.dump(self.TF_IDF_MATRIX, f)
             f.close()
 
-    def compute_cosine_similarity(self, query: str) -> np.ndarray:
+    def compute_cosine_similarity(self, query: str):
         """
         Calcule la similarité cosinus entre la requête et les sous-titres
         :param query: requête de l'utilisateur
@@ -81,33 +77,45 @@ class tfIDF:
         """
         try:
             query_vector = self.VECTORIZER.transform([query])
-            return cosine_similarity(query_vector, self.TF_IDF_MATRIX)
+            result = cosine_similarity(query_vector, self.TF_IDF_MATRIX)
+            return result
         except Exception as e:
             print(e)
 
     def get_series_similaires(self, query: str):
         """
-        Retourne les série similaires à la requête
+        Retourne les séries similaires à la requête
+        :param query: requête de l'utilisateur
         """
-        similarities = self.compute_cosine_similarity(query)
-        series_indices = similarities.argsort()[0][::-1]
-        series = []
+        try:
+            similarities = self.compute_cosine_similarity(query)
+            series_indices = similarities.argsort()[0][::-1]
+        except Exception as e:
+            print(e)
+            return []
 
+        series = []
         for idx in series_indices:
             series.append(self.SERIES_NAMES[idx])
-
         return series
+
+    def getShapes(self):
+        return self.TF_IDF_MATRIX.shape
 
 
 if __name__ == '__main__':
     # Création d'un objet tf_idf
-    tiIDF = tfIDF('../data/data.json', '../data/series_names.json', 'VF')
+    tiIDF_VF = tfIDF('../data/data_VF.json', '../data/series_names_VF.json', 'VF')
+    tiIDF_VO = tfIDF('../data/data_VO.json', '../data/series_names_VO.json', 'VO')
+
+    print("shape de la matrice VF: ", tiIDF_VF.getShapes())
+    print("shape de la matrice VO: ", tiIDF_VO.getShapes())
 
     # Requête de l'utilisateur
     query = input('Requête: ')
 
     # Récupération des séries similaires
-    series_similaires = tiIDF.get_series_similaires(query)
+    series_similaires = tiIDF_VF.get_series_similaires(query)
 
     # Affichage des séries similaires
     for i in range(5):
