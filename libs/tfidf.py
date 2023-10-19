9d7fbddf19d53ccdd1cd3dce70ba53c97151810d
Maxiwere 45 <amdjad9857@gmail.com>
2023-10-19 10:50:09 +0200

    Optimisation de la fonction TF-IDF

--- libs/tfidf.py
+++ libs/tfidf.py
@@ -13,8 +13,11 @@ class tfIDF:
     def __init__(self, data, series_names, type_series):
         st = time.time()
         self.VECTORIZER = TfidfVectorizer()
+        self.series_names = list()
         with open(series_names, 'r', encoding='utf-8') as f:
-            self.SERIES_NAMES = json.load(f)
+            self.SERIES_INFOS = json.load(f)
+            for serie in self.SERIES_INFOS:
+                self.series_names.append(serie['name'].lower())
         with open(data, 'r') as f:
             try:
                 self.TF_IDF_MATRIX = self.VECTORIZER.fit_transform(json.load(f))
@@ -35,15 +38,6 @@ class tfIDF:
             result = chardet.detect(f.read())
         return str(result['encoding'])
 
-    def save_data(self, file_name):
-        """
-        Sauvegarde les données
-        :param file_name: nom du fichier
-        """
-        with open(file_name, 'w', encoding='utf-8') as f:
-            json.dump(self.TF_IDF_MATRIX, f)
-            f.close()
-
     def compute_cosine_similarity(self, query: str):
         """
         Calcule la similarité cosinus entre la requête et les sous-titres
@@ -60,24 +54,30 @@ class tfIDF:
         :param query: requête de l'utilisateur
         """
         try:
-            similarities = self.compute_cosine_similarity(query)
-            series_indices = similarities.argsort()[0][::-1]
+            if query.lower() in self.series_names:
+                return [self.SERIES_INFOS[self.series_names.index(query.lower())]]
+            else:
+                similarities = self.compute_cosine_similarity(query)
+                series_indices = similarities.argsort()[0][::-1]
         except Exception as e:
             print(e)
             return []
-        return [self.SERIES_NAMES[idx] for idx in series_indices[:5]]
+        return [self.SERIES_INFOS[idx] for idx in series_indices[:5]]
 
     def getShapes(self):
         return self.TF_IDF_MATRIX.shape
+
+    def getSeriesNames(self):
+        return self.SERIES_INFOS
 
 
 if __name__ == '__main__':
     # Création d'un objet tf_idf
     tiIDF_VF = tfIDF('../data/data_VF.json', '../data/seriesInfos.json', 'VF')
-    # tiIDF_VO = tfIDF('../data/data_VO.json', '../data/seriesInfos.json', 'VO')
+    tiIDF_VO = tfIDF('../data/data_VO.json', '../data/seriesInfos.json', 'VO')
 
     print("shape de la matrice VF: ", tiIDF_VF.getShapes())
-    # print("shape de la matrice VO: ", tiIDF_VO.getShapes())
+    print("shape de la matrice VO: ", tiIDF_VO.getShapes())
 
     while True:
         # Requête de l'utilisateur
@@ -85,5 +85,5 @@ if __name__ == '__main__':
         # Récupération des séries similaires
         series_similaires = tiIDF_VF.get_series_similaires(queryUser)
         # Affichage des séries similaires
-        for i in range(5):
+        for i in range(len(series_similaires)):
             print(series_similaires[i])
