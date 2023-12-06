225669c87c8456398d3d2ff871f40f7810ae9155
Amdjad <amdjad9857@gmail.com>
2023-12-06 21:18:47 +0100

    MAJ index.html

--- libs/tfidf.py
+++ libs/tfidf.py
@@ -2,21 +2,20 @@ import chardet
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
-import time
 import mmap
+import threading
+import time
 
 
 class tfIDF:
     """
     Classe permettant de calculer la similarité cosinus entre une requête et les sous-titres
     """
-
     def __init__(self, data, series_names, type_series):
         st = time.time()
         self.__VECTORIZER__ = TfidfVectorizer()
         self.series_names = list()
         with open(series_names, 'r', encoding='utf-8') as f:
-
             mmap_file = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) # Accélération de la lecture du fichier
             self.SERIES_INFOS = json.load(mmap_file)
             for serie in self.SERIES_INFOS:
@@ -30,7 +29,6 @@ class tfIDF:
         self.type = type_series
         end = time.time()
         print(f'Sous-titres {type_series} OK, temps de chargement: {round(end - st, 2)}s')
-
     @staticmethod
     def detect_encoding(file_path) -> str:
         """
@@ -77,8 +75,7 @@ class tfIDF:
 if __name__ == '__main__':
     # Création d'un objet tf_idf
     tiIDF_VF = tfIDF('../data/data_vf.json', '/home/maxiwere45/PycharmProjects/seriesNet/data/seriesInfos.json', 'VF')
-    #tiIDF_VO = tfIDF('../data/data_vo.json', '../data/seriesInfos.json', 'VO')
-
+    tiIDF_VO = tfIDF('../data/data_vo.json', '../data/seriesInfos.json', 'VO')
     #print("shape de la matrice VF: ", tiIDF_VF.getShapes())
     #print("shape de la matrice VO: ", tiIDF_VO.getShapes())
 
